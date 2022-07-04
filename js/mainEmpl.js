9adcdc2ac5709e84ac73d4a2af1d0c55ceaf089e
F2Pl4y <fernando.hurtado600@hotmail.com>
2022-07-03 21:19:26 -0500

    asdasdas

--- js/mainEmpl.js
+++ js/mainEmpl.js
@@ -1,5 +1,5 @@
-// const dominio2 = "http://f3rn4nd021py.pythonanywhere.com/";
-const dominio2 = "http://127.0.0.1:5000/";
+const dominio2 = "http://f3rn4nd021py.pythonanywhere.com/";
+// const dominio2 = "http://127.0.0.1:5000/";
 window.addEventListener('load', (e) => {
     const url = window.location.pathname;
     const boton1 = document.getElementById('btnEnviarE');

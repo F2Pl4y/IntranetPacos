21ee333e0f248322a7decf27f05376024af06793
F2Pl4y <fernando.hurtado600@hotmail.com>
2022-07-10 11:54:37 -0500

    sadsa

--- js/efectoAnimacion.js
+++ js/efectoAnimacion.js
@@ -1,12 +1,12 @@
-// function menuResponsive() { let a = document.getElementById("btnMenu"), b = document.getElementById("sidenav"); a.addEventListener("click", a => { b.classList.toggle("responsive") }) } function filtroTablaTrabajadores() { $(document).ready(function () { $("#inputTrabajador").on("keyup", function () { var a = $(this).val().toLowerCase(); $("#contenido2 tr").filter(function () { $(this).toggle($(this).text().toLowerCase().indexOf(a) > -1) }) }) }) } window.addEventListener("load", a => { menuResponsive(), filtroTablaTrabajadores() })
-// function menuResponsive() {
-//     let a = document.getElementById("btnMenu");
-//     let b = document.getElementById("sidenav");
-//     a.addEventListener("click", a => {
-//         b.classList.toggle("responsive")
-//     }
-//     )
-// }
+function menuResponsive() { let a = document.getElementById("btnMenu"), b = document.getElementById("sidenav"); a.addEventListener("click", a => { b.classList.toggle("responsive") }) } function filtroTablaTrabajadores() { $(document).ready(function () { $("#inputTrabajador").on("keyup", function () { var a = $(this).val().toLowerCase(); $("#contenido2 tr").filter(function () { $(this).toggle($(this).text().toLowerCase().indexOf(a) > -1) }) }) }) } window.addEventListener("load", a => { menuResponsive(), filtroTablaTrabajadores() })
+function menuResponsive() {
+    let a = document.getElementById("btnMenu");
+    let b = document.getElementById("sidenav");
+    a.addEventListener("click", a => {
+        b.classList.toggle("responsive")
+    }
+    )
+}
 function filtroTablaTrabajadores() {
     $(document).ready(function () {
         $("#inputTrabajador").on("keyup", function () {

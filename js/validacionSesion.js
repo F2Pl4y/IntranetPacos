f85babf1313ffad0feb28fa624f90f203dae4e87
F2Pl4y <fernando.hurtado600@hotmail.com>
2022-07-15 14:31:05 -0500

    adasd

--- js/validacionSesion.js
+++ js/validacionSesion.js
@@ -1,5 +1,5 @@
-// const dominioSesion = "https://f3rn4nd021py.pythonanywhere.com/";
-const dominioSesion = "http://127.0.0.1:5000/";
+const dominioSesion = "https://f3rn4nd021py.pythonanywhere.com/";
+// const dominioSesion = "http://127.0.0.1:5000/";
 window.addEventListener('load', (e) => {
     const url = window.location.pathname;
 
